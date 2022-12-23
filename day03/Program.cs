1676a444f0088c75eb288b4dc5c620ef2038885a
Serge Mulders <sergemulders@Serges-MacBook-Air.local>
2022-12-23 19:53:07 +0100

    day03 part 1

---
+++ day03/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var path = Path.Combine(Environment.CurrentDirectory, "data");
+var rucksacks = File.ReadAllLines(path);
+var priority = 0;
+
+foreach (var r in rucksacks)
+{
+    var left = r.Substring(0, r.Length / 2);
+    var right = r.Substring(r.Length / 2);
+
+    var duplicate = left.Where(x => right.Contains(x)).Distinct().Single();
+
+    priority += GetPrio(duplicate);
+
+}
+
+int GetPrio(char c)
+{
+    if (char.IsLower(c))
+        return (int)c - 96;
+    else
+        return (int)c - 64+26;
+}
+
+Console.WriteLine($"Priority {priority}");
