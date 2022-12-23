71c2c3544c40f54fc50507d1832913361975c5fb
Serge Mulders <sergemulders@Serges-MacBook-Air.local>
2022-12-23 22:43:23 +0100

    day04 part 1

---
+++ day04/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var path = Path.Combine(Environment.CurrentDirectory, "data");
+var data = File.ReadAllLines(path);
+
+var count = data.ToList().Where(x => overlaps(x)).Count();
+
+Console.WriteLine($"With overlap {count}");
+
+bool overlaps(string pair)
+{
+    // 6-6,4-6
+
+    var one = pair.Split(",").First().Split("-").Select(x=> int.Parse(x));
+    var two = pair.Split(",").Last().Split("-").Select(x=> int.Parse(x));
+
+    // one in two
+    return ((one.ElementAt(0) >= two.ElementAt(0) && one.ElementAt(1) <= two.ElementAt(1)) ||
+     // two in one 
+     two.ElementAt(0) >= one.ElementAt(0) && two.ElementAt(1) <= one.ElementAt(1));
+}
