d5bdeb505d7dc6c21216fceb9898516536a20383
Serge Mulders <sergemulders@Serges-MacBook-Air.local>
2023-01-21 11:59:00 +0100

    day 06 part 1

---
+++ day06/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var path = Path.Combine(Environment.CurrentDirectory, "data");
+var data = File.ReadAllText(path);
+
+var markerStart = 0;
+for (int i = 0; i < data.Length - 4; i++)
+{
+    var marker = data.Substring(i, 4);
+    if (marker.Distinct().Count() == 4)
+    {
+        markerStart = i + 4;
+        break;
+    }
+
+}
+
+Console.WriteLine($"Marker start: {markerStart}");
