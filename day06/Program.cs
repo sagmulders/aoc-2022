86ffdba097de5c666b28c93ab7a0fdea78041ee3
Serge Mulders <sergemulders@Serges-MacBook-Air.local>
2023-01-21 12:06:07 +0100

    day 06 part 2

--- day06/Program.cs
+++ day06/Program.cs
@@ -5,12 +5,15 @@ var path = Path.Combine(Environment.CurrentDirectory, "data");
 var data = File.ReadAllText(path);
 
 var markerStart = 0;
-for (int i = 0; i < data.Length - 4; i++)
+var messages=0;
+
+for (int i = 0; i < data.Length - 14; i++)
 {
-    var marker = data.Substring(i, 4);
-    if (marker.Distinct().Count() == 4)
+    // find marker
+    var marker = data.Substring(i, 14);
+    if (marker.Distinct().Count() == 14)
     {
-        markerStart = i + 4;
+        markerStart = i + 14;
         break;
     }
 
